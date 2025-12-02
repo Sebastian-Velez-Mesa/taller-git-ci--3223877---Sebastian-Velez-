9692f7a0abe959c907954ef98413516b0b84be2e
Sebastian-Velez-Mesa <sebas.0927.svm@gmail.com>
2025-12-02 10:10:24 -0500

    feat: agregar boton y lista en la UI

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 function suma(a, b) {
     return a + b;
 }
+
+function agregarItem(texto) {
+    const lista = document.getElementById('lista');
+    const nuevoItem = document.createElement('li');
+    nuevoItem.textContent = texto;
+    lista.appendChild(nuevoItem);
+}
+
 console.log("App lista")
